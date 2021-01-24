d3d20f3b08b08f84bfc32b728760fb9f9a11f170
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-01-25 03:05:38 +0500

    Update App.js

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{color: "white",fontSize: 40 }}>Counter : {num}</Text>
-      <Button title="Click" onPress={()=> setNum(num++)}></Button>
+      <Button title="Click" onPress={()=> setNum(++num)}></Button>
       <Image style={{width: 200, height: 200}} source={{uri: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-tulips-blooming-in-field-royalty-free-image-1584131616.jpg?crop=0.630xw:1.00xh;0.186xw,0&resize=640:*'}}></Image>
       <StatusBar style="auto" />
     </View>
