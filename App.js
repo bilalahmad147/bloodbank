868792ce50187f534be8dfa4fc9055a5e92204f2
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-01-25 21:54:20 +0500

    Create register screen

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import Welcome from './src/Components/Welcome'
+import Welcome from './src/Components/Welcome';
+import Register from './src/Components/Register';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      <Welcome />
+      <Register />
       <StatusBar style="auto" />
     </View>
   );
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,28 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Text, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const Welcome = () => {
 
     return (
-        <View style={styles.container}>
+        <View>
             <TextInput
-                style={{ height: 40 }}
-                placeholder="Type here to translate!"
+                style={styles.input}
+                placeholder="Enter user name.."
             />
-            <Button title="Register YourSelf"></Button>
+            <TextInput
+                style={styles.input}
+                placeholder="Enter password.."
+            />
+            <TouchableOpacity style={styles.btn}>
+                <Text style={styles.btnText}><Icon name="plus" size={20} /> Login</Text>
+            </TouchableOpacity>
             <StatusBar style="auto" />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
+    input: {
+        height: 60,
+        padding: 8,
+        fontSize: 16,
     },
+    btn: {
+        backgroundColor: "#c2bad8",
+        padding: 9,
+        margin: 5,
+    },
+    btnText: {
+        color: "darkslateblue",
+        fontSize: 20,
+        textAlign: 'center',
+    }
 });
 
 export default Welcome;
