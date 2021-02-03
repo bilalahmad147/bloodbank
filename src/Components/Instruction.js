aba9447593dc904887ef04e3da30fefc84f7ae4e
Bilal Ahmad <61399708+bilalahmad147@users.noreply.github.com>
2021-02-03 08:37:03 +0500

    final commit

--- src/Components/Instruction.js
+++ src/Components/Instruction.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 const Instructions = () => {
 
@@ -9,12 +9,16 @@ const Instructions = () => {
         <View style={styles.container}>
             <ScrollView>
                 <Text style={styles.text}><Icon name="circle" size={16} /> Welcome to blood bank app</Text>
-                <Text style={styles.text}><Icon name="circle" size={16} /> You have to follw these steps for using this blood bank app.</Text>
-                <Text style={styles.text}><Icon name="circle" size={16} /> First, you have to register youself using email and password.</Text>
-                <Text style={styles.text}><Icon name="circle" size={16} /> You can register by using facebook login.</Text>
+                <Text style={styles.text}><Icon name="circle" size={16} /> You have to follow these steps for using this blood bank app.</Text>
+                <Text style={styles.text}><Icon name="circle" size={16} /> First, you have to signUp youself using information.</Text>
+                <Text style={styles.text}><Icon name="circle" size={16} /> If you have already an account, you can simply login and proceed. </Text>
                 <Text style={styles.text}><Icon name="circle" size={16} /> You have to choose one option. Either you will be a donor or blood seeker.</Text>
                 <Text style={styles.text}><Icon name="circle" size={16} /> For donating blood you have to submit your details like name, age, blood group and location.</Text>
                 <Text style={styles.text}><Icon name="circle" size={16} /> For finding blood, You have to search blood group and city name so that we can show further detail about donor.</Text>
+                <Text style={styles.text}><Icon name="circle" size={16} /> Below given chart will help you for understanding blood groups.</Text>
+                <View style={{ flex: 1, padding: 30, marginTop: 30, alignItems: 'center', marginBottom: 50 }}>
+                    <Image style={{ width: 270, height: 170 }} source={require('./imgs/bloodChart.png')} />
+                </View>
             </ScrollView>
             <StatusBar style="auto" />
         </View>
--- src/Components/Redux/Actions/action.js
+++
@@ -1,8 +0,0 @@
-export const ADD_DETAIL = "ADD_DETAIL";
-
-export const addDetail = (name, price, imgUrl) => ({
-    type: ADD_DETAIL,
-    payload: {
-        name, price, imgUrl
-    }
-})
